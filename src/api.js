0c423ddea22eb16692308965673ec1a137b61152
Antonio Jurić <juricantonio18@gmail.com>
2024-07-24 11:14:09 +0200

    Added initial api setup

---
+++ src/api.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+
+
+app.get("/blockchain", (req, res, next) => {});
+
+app.get("/mine", (req, res, next) => {});
+
+app.post("/transaction", (rew, res, next) => {});
+
+
+
+app.listen(3000);
