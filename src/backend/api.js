33a6be31ec48d1abef3d258aa25418655e42f1ce
Antonio Jurić <juricantonio18@gmail.com>
2024-07-24 11:49:46 +0200

    Added body-parser

--- src/backend/api.js
+++ src/backend/api.js
@@ -1,5 +1,10 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = 3000;
 
@@ -7,7 +12,10 @@ app.get("/blockchain", (req, res, next) => {});
 
 app.get("/mine", (req, res, next) => {});
 
-app.post("/transaction", (rew, res, next) => {});
+app.post("/transaction", (req, res, next) => {
+  console.log(req.body);
+  res.send(`The amount sent: ${req.body.amount} duhcoins`);
+});
 
 app.listen(3000, () => {
   console.log("SERVER started on PORT " + port);
